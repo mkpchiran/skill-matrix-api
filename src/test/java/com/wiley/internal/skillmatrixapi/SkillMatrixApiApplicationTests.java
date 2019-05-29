1ce9e5b35786ff77a0a4fcc58832b054b0d0a741
Chiranthana <pchirantha@wiley.com>
2019-05-29 12:37:14 +0530

    Update unit test

--- src/test/java/com/wiley/internal/skillmatrixapi/SkillMatrixApiApplicationTests.java
+++ src/test/java/com/wiley/internal/skillmatrixapi/SkillMatrixApiApplicationTests.java
@@ -5,12 +5,15 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.wiley.internal.apps.SkillMatrixApplication;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes={SkillMatrixApplication.class})
 public class SkillMatrixApiApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	
 	}
 
 }
