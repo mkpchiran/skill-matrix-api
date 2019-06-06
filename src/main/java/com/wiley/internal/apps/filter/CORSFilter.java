45a6534fd98ca966a11c065af7d41d7bcbdcedd7
Chiranthana <pchirantha@wiley.com>
2019-06-06 09:44:01 +0530

    add coockie to allow header

--- src/main/java/com/wiley/internal/apps/filter/CORSFilter.java
+++ src/main/java/com/wiley/internal/apps/filter/CORSFilter.java
@@ -25,7 +25,7 @@ public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
     response.setHeader("Access-Control-Allow-Credentials", "true");
     response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
     response.setHeader("Access-Control-Max-Age", "3600");
-    response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me, auth, Authorization");
+    response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me, auth, Authorization,Cookie");
 
     chain.doFilter(req, res);
 }
