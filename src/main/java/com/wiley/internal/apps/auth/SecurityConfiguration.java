9f0e2866fe19b6c62eb40cd3fb56c70626781610
Chiranthana <pchirantha@wiley.com>
2019-06-06 10:28:43 +0530

    Update cors filter

--- src/main/java/com/wiley/internal/apps/auth/SecurityConfiguration.java
+++ src/main/java/com/wiley/internal/apps/auth/SecurityConfiguration.java
@@ -8,16 +8,20 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.session.SessionManagementFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.cors().and()
+        httpSecurity
+        .addFilterBefore(corsFilter(), SessionManagementFilter.class)
+        .cors().and()
         	.authorizeRequests().antMatchers("/login").permitAll().and()
             .authorizeRequests().antMatchers("/h2-console/**").permitAll().and()
             .authorizeRequests().antMatchers("/v1/**").authenticated();
@@ -41,4 +45,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		source.registerCorsConfiguration("/**", configuration);
 		return source;
 	}
+    
+    @Bean
+    CorsFilter corsFilter() {
+        CorsFilter filter = new CorsFilter(corsConfigurationSource());
+        return filter;
+    }
 }
